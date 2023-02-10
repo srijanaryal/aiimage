0871864eb9e0f589af91a5c1482e0f2a40b505fd
unknown <srijanaryal5@gmail.com>
2023-02-10 20:55:09 +0545

    class made and fetched http

--- lib/imagecontroller.dart
+++ lib/imagecontroller.dart
@@ -1,5 +1,37 @@
+import 'dart:convert';
+
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 
 class ImageController extends GetxController {
   var url = Uri.parse(' https://api.openai.com/v1/images/generations');
+  var api_token = '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';
+  final data = ''.obs;
+  final isLoading = false.obs;
+
+  Future getImage({required String imageText}) async {
+    try {
+      isLoading.value = true;
+      var request = await http.post(url,
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer $api_token'
+          },
+          body: jsonEncode({
+            'prompt': imageText,
+            'n': 1,
+          }));
+      if (request.statusCode == 200) {
+        isLoading.value = false;
+        data.value = jsonDecode(request.body)['data'][0]['url'];
+        print(data..value);
+      } else {
+        isLoading.value = false;
+        print(jsonDecode(request.body));
+      }
+    } catch (e) {
+      isLoading.value = false;
+      print(e.toString());
+    }
+  }
 }
